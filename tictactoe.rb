7836f59fb749ab2d051bbb73499ce75a96223723
Bryan Luu <loc.bryan.luu@gmail.com>
2020-06-11 17:52:03 -0700

    Getting close to finishing TTT, have win conditions set up.

--- tictactoe.rb
+++ tictactoe.rb
@@ -23,6 +23,7 @@ end
 # Tic-Tac-Toe board
 class Board
   def initialize
+    @turn_number = 0
     @board = [[], [], []]
     for i in 0...3
       for j in 0...3
@@ -41,6 +42,102 @@ class Board
       str += "\n"
     end
     str
+  end
+
+  # checks whether coordinate is valid
+  def valid_coord?(row, col)
+    (0...3).include?(row) && (0...3).include?(col)
+  end
+
+  def set_x(row, col)
+    @turn_number += 1
+    set_to_symbol('x', row, col)
+  end
+
+  def set_o(row, col)
+    set_to_symbol('o', row, col)
+  end
+
+  def winner
+    return nil unless @turn_number >= 3
+
+    directions = %i[horizontal vertical
+                    diagonal antidiagonal]
+    victor = nil
+    for dir in directions
+      victor = check_win(dir)
+      if victor
+        return victor
+      end
+    end
+  end
+
+  private
+
+  def set_to_symbol(symbol, row, col)
+    return puts 'Out of range' unless valid_coord?(row, col)
+
+    if @board[row][col].empty?
+      @board[row][col].symbol = symbol
+    else
+      puts 'Square already has symbol!'
+      return nil
+    end
+  end
+
+  def check_row_equal(row)
+    if @board[row][0].symbol == @board[row][1].symbol &&
+       @board[row][0].symbol == @board[row][2].symbol
+      return @board[row][0].symbol
+    else
+      return nil
+    end
+  end
+
+  def check_col_equal(col)
+    if @board[0][col].symbol == @board[1][col].symbol &&
+       @board[0][col].symbol == @board[2][col].symbol
+      return @board[0][col].symbol
+    else
+      return nil
+    end
+  end
+
+  def check_diagonal_equal
+    if @board[0][0].symbol == @board[1][1].symbol &&
+       @board[0][0].symbol == @board[2][2].symbol
+      return @board[0][0].symbol
+    else
+      return nil
+    end
+  end
+
+  def check_antidiagonal_equal
+    if @board[2][0].symbol == @board[1][1].symbol &&
+       @board[2][0].symbol == @board[0][2].symbol
+      return @board[1][1].symbol
+    else
+      return nil
+    end
+  end
+
+  def check_win(direction)
+    victor = nil
+    # check along direction
+    for i in 0...3
+      victor = case direction
+               when :horizontal then check_row_equal(i)
+               when :vertical then check_col_equal(i)
+               when :diagonal then check_diagonal_equal
+               when :antidiagonal then check_antidiagonal_equal
+               end
+      if victor # if victor is found
+        return victor
+      elsif direction == :diagonal || direction == :antidiagonal
+        return nil
+      end
+    end
+    return nil
   end
 end
 
