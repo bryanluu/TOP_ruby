5dbe1bc8ed4ececc06b2d51f68f8f71973da9e10
Bryan Luu <loc.bryan.luu@gmail.com>
2020-06-15 15:19:10 -0700

    Added feedback to Mastermind in Ruby.

--- mastermind.rb
+++ mastermind.rb
@@ -11,29 +11,53 @@ class Code
 
   attr_reader :row
 
-  def initialize(colors)
-    @row = colors
+  def initialize(code)
+    @row = []
+    code.each { |x| @row << COLORS[x] }
   end
-end
 
-class Key
-  COLORS = { 0 => :black,
-             1 => :white }.freeze
+  def to_s
+    @row.to_s
+  end
+
+  # returns a counter hash
+  def count
+    map = {}
+    COLORS.values.each { |v| map[v] = 0 }
+    @row.each do |x|
+      map[x] = map[x] + 1
+    end
+    map
+  end
+
+  def compare(code)
+    feedback = []
+    check = code.row[0..-1]
+    count.each do |k, v|
+      # push a white peg for all pegs included in code
+      (0...v).each { feedback << 1 and check.delete_at(check.index(k)) if check.include?(k) }
+    end
+    code.row.each_with_index do |x, i|
+      # prepend a black peg and remove a white peg for correct pegs in code
+      feedback.unshift(0) and feedback.pop if x == @row[i]
+    end
+    feedback.map! { |x| COLORS[x] } # map to colors
+  end
 end
 
 # Class that represents a Decoding Board
 class Board
   def initialize(code)
-    @code = code
+    @code = Code.new(code)
   end
 
   def get_feedback(code)
-    cmp = []
-    code.each_with_index do |x, i|
-      cmp.push(x) if x == @code[i]
-    end
-    puts cmp
+    @code.compare(code)
   end
 end
 
-binding.pry
+b = Board.new([1, 1, 1, 0])
+c = Code.new([1, 1, 0, 1])
+p b.get_feedback(c)
+
+# binding.pry
