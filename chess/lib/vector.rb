632ec6031fe22dc1b0bdec40c832a7aadbf6d1ac
Bryan Luu <loc.bryan.luu@gmail.com>
2020-07-05 10:31:30 -0700

    Implemented vector class (and passing tests) for Chess.

---
+++ chess/lib/vector.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+class Vector
+  def initialize(array)
+    @data = array[0..-1]
+  end
+
+  def [](*args)
+    @data.[](*args)
+  end
+
+  def ==(other)
+    @data == other.to_a
+  end
+
+  def +(other)
+    raise TypeError, 'Uneven length vectors added!' unless ndim == other.ndim
+
+    result = Array.new(ndim) { |i| @data[i] + other[i] }
+    Vector.new(result)
+  end
+
+  def -(other)
+    raise TypeError, 'Uneven length vectors added!' unless ndim == other.ndim
+
+    result = Array.new(ndim) { |i| @data[i] - other[i] }
+    Vector.new(result)
+  end
+
+  def ndim
+    @data.length
+  end
+
+  def to_s
+    @data.inspect
+  end
+
+  def to_a
+    @data[0..-1]
+  end
+
+  def inspect
+    "\#<#{ndim}-dim. Vector: " + @data.inspect + '>'
+  end
+end
--- chess/spec/vector_spec.rb
+++ chess/spec/vector_spec.rb
@@ -11,6 +11,32 @@ describe Vector do
       expect(v.to_a).to eq(array)
     end
   end
+  describe '#[]' do
+    it 'correctly indices into data array' do
+      dim = rand(2..5)
+      array = Array.new(dim) { rand(0..10) }
+      v = Vector.new(array)
+      i = rand(0...dim)
+      expect(v[i]).to be(array[i])
+    end
+  end
+  describe '#==' do
+    it 'correctly returns true' do
+      dim = rand(1..5)
+      a = Array.new(dim) { rand(0..10) }
+      u = Vector.new(a)
+      v = Vector.new(a)
+      expect(u).to eq(v)
+    end
+    it 'correctly returns false' do
+      dim = rand(1..5)
+      a = Array.new(dim) { rand(0..10) }
+      u = Vector.new(a)
+      a[0] = -1
+      v = Vector.new(a)
+      expect(u).not_to eq(v)
+    end
+  end
   describe '#+' do
     it 'adds two same-size vectors correctly' do
       dim = rand(1..5)
@@ -28,15 +54,15 @@ describe Vector do
       u = Vector.new(a)
       b = Array.new(dim) { 'abcdefghijklmnopqrstuvwxyz'.split('').sample }
       v = Vector.new(b)
-      expect(u + v).to raise_error
+      expect{ u + v }.to raise_error(TypeError)
     end
     it 'complains when uneven vectors are added' do
       dim = rand(1..5)
       a = Array.new(dim) { rand(0..10) }
       u = Vector.new(a)
-      b = Array.new(dim) { rand(0..10) }
+      b = Array.new(dim + 1) { rand(0..10) }
       v = Vector.new(b)
-      expect(u + v).to raise_error
+      expect{ u + v }.to raise_error(TypeError)
     end
   end
   describe '#-' do
@@ -56,15 +82,23 @@ describe Vector do
       u = Vector.new(a)
       b = Array.new(dim) { 'abcdefghijklmnopqrstuvwxyz'.split('').sample }
       v = Vector.new(b)
-      expect(u - v).to raise_error
+      expect{ u - v }.to raise_error(TypeError)
     end
     it 'complains when uneven vectors are subtracted' do
       dim = rand(1..5)
       a = Array.new(dim) { rand(0..10) }
       u = Vector.new(a)
-      b = Array.new(dim) { rand(0..10) }
+      b = Array.new(dim + 1) { rand(0..10) }
       v = Vector.new(b)
-      expect(u - v).to raise_error
+      expect{ u - v }.to raise_error(TypeError)
+    end
+  end
+  describe '#ndim' do
+    it 'correctly returns length of data array' do
+      dim = rand(1..5)
+      a = Array.new(dim) { rand(0..10) }
+      v = Vector.new(a)
+      expect(v.ndim).to be(dim)
     end
   end
   describe '#to_s' do
@@ -81,7 +115,7 @@ describe Vector do
       dim = rand(1..5)
       array = Array.new(dim) { rand(0..10) }
       v = Vector.new(array)
-      str = "{#{array.length}-dim. Vector: " + array.inspect + '}'
+      str = "\#<#{array.length}-dim. Vector: " + array.inspect + '>'
       expect(v.inspect).to eq(str)
     end
   end
