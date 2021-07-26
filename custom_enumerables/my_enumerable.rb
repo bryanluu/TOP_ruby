f06f2015defd727a583ea867716904a99e93b7f1
Bryan Luu <loc.bryan.luu@gmail.com>
2021-07-25 17:18:25 -0700

    Finished my_each_with_index

--- custom_enumerables/my_enumerable.rb
+++ custom_enumerables/my_enumerable.rb
@@ -2,14 +2,18 @@
 # frozen_string_literal: true
 
 module Enumerable
-  def my_each(&block)
+  def my_each
     for item in self
       yield item
     end
   end
 
   def my_each_with_index
-
+    i = 0
+    for item in self
+      yield item, i
+      i += 1
+    end
   end
 end
 
@@ -21,13 +25,19 @@ def compare(method)
   case method
   when "each"
     block = Proc.new { |x| puts x }
+  when "each_with_index"
+    block = Proc.new { |x, i| puts "#{x}, #{i}" }
   else
     puts "my_#{method} unsupported"
     return
   end
-  puts numbers.send("my_#{method}", &block)
+  result = numbers.send("my_#{method}", &block)
+  puts "="
+  puts result
   puts "---"
-  puts numbers.send(method, &block)
+  result = numbers.send(method, &block)
+  puts "="
+  puts result
 end
 
 if __FILE__ == $PROGRAM_NAME
