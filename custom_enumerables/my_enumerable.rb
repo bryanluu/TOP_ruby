51a76641ee5e4cb7e7140890e8a14c8816af6796
Bryan Luu <loc.bryan.luu@gmail.com>
2021-07-25 17:31:59 -0700

    Finished my_select

--- custom_enumerables/my_enumerable.rb
+++ custom_enumerables/my_enumerable.rb
@@ -15,6 +15,16 @@ module Enumerable
       i += 1
     end
   end
+
+  def my_select
+    result = self.class.new
+    my_each do |item|
+      if yield(item)
+        result.push item
+      end
+    end
+    result
+  end
 end
 
 def compare(method)
@@ -27,6 +37,8 @@ def compare(method)
     block = Proc.new { |x| puts x }
   when "each_with_index"
     block = Proc.new { |x, i| puts "#{x}, #{i}" }
+  when "select"
+    block = Proc.new { |x| x % 2 == 0 }
   else
     puts "my_#{method} unsupported"
     return
